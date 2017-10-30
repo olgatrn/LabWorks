802b1281b830f3a4eced012ac7979cef74af16a9
olgatrn <taran.olya@gmail.com>
2017-10-30 23:42:32 +0200

    LabWorks 1-2-1, 1-2-2

---
+++ src/LabWork121.java
@@ -0,0 +1,27 @@
+public class LabWork121 {
+    public static void main(String[] args){
+        System.out.println("  *****");
+        System.out.println("      *");
+        System.out.println("      *");
+        System.out.println("*     *");
+        System.out.println(" *****");
+        System.out.println();
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *******");
+        System.out.println("*       *");
+        System.out.println();
+        System.out.println("*       *");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+        System.out.println();
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *******");
+        System.out.println("*       *");
+    }
+}
---
+++ src/LabWork122.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LabWork122 {
+    public static void main(String[] args){
+        System.out.print("Please enter the name of the student: ");
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.print(line);
+    }
+}
