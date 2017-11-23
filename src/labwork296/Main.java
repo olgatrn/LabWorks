82daf3974a09158b90b16a69d9f169f5ca326e47
olgatrn <taran.olya@gmail.com>
2017-11-24 00:53:33 +0200

    hometask pattern

--- src/labwork296/Main.java
+++ src/labwork296/Main.java
@@ -1,18 +1,19 @@
-package labwork295.testregexp.com.brainacad.oop.testsregexp;
+package labwork296;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arrayOfUserNames = {"VOVA", "Ivan", "R2d2", "ZX", "Anna", "12345", "ToAd", "TomCat", ""};
+        String[] arrayOfUserNames = {"3456,67", "4,678,345,546.890754", "7,5", "6.0",
+                "5,435.67", "543", "ToAd", "5435,56", "."};
         for (String userName : arrayOfUserNames) {
             System.out.println(checkPersonWithRegExp(userName));
         }
     }
 
     public static boolean checkPersonWithRegExp(String userNameString) {
-        Pattern pattern = Pattern.compile("[A-Z][a-z]+");
+        Pattern pattern = Pattern.compile("(\\d{1,3}(,(\\d{3}))*[.]\\d+)|(\\d+[.,]\\d+)");
         Matcher matcher = pattern.matcher(userNameString);
         if (matcher.matches()) {
             return true;
